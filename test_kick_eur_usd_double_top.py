29cad0341f0675c3832dac3239276ba4784db0c6
OgamiTakeru <takeru_kagerou@hotmail.co.jp>
2026-08-30 19:11:50 +0900

    ダブルトップの通貨別起動ファイルを追加

---
+++ test_kick_eur_usd_double_top.py
@@ -0,0 +1,23 @@
+# 最新更新日時: 2026-08-30 19:11 JST
+"""EUR_USDのDoubleTop条件探索と翌年固定リプレイを起動する。"""
+
+import datetime
+
+from double_top_grid_validation import run_pair
+
+
+PAIR = "EUR_USD"
+TRAIN_START = datetime.datetime(2023, 7, 30, 0, 0, 0)
+TRAIN_END = datetime.datetime(2025, 7, 30, 0, 0, 0)
+OOS_START = datetime.datetime(2025, 7, 30, 0, 0, 0)
+OOS_END = datetime.datetime(2026, 7, 30, 0, 0, 0)
+
+
+if __name__ == "__main__":
+    run_pair(
+        PAIR,
+        TRAIN_START,
+        TRAIN_END,
+        OOS_START,
+        OOS_END,
+    )
